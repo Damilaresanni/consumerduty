4ec911d80c498a0766db928a7a88b9761c9d57b2
damilaresanni <sannidamilare9@gmail.com>
2026-02-05 20:14:26 +0000

    added fields for consumer review table

--- monitoring/models.py
+++ monitoring/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from product.models import Product
 
-# Create your models here.
+
+
+class VulnerableOptions(models.TextChoices):
+    yes = "yes", "Yes"
+    no = "no", "No"
+    mixed = "mixed", "Mixed"
+    
+    
+class StatusLevel(models.TextChoices):
+    low = "low", "Low"
+    mid = "mid", "Mid"
+    high = "high", "High"
+    
+    
+class ConsumerDutyReview(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="consumer_duty_reviews"
+    )
+
+    review_frequency = models.PositiveIntegerField()
+    last_consumer_duty_review_date = models.DateTimeField()
+    notes_on_known_risks = models.TextField(null=True, blank=True)
+    
+    
+    vulnerability_consideration = models.CharField(
+        max_length=255,
+        choices=VulnerableOptions.choices,
+        null = True,
+        blank = True
+    )
+
+    def __str__(self):
+        return f"{self.product.product_name} Review"
